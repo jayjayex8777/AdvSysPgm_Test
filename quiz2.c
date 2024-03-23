11fe36012b86b06a2f89041f51e5715145078103
Intae Jun <jayjayex8777@gmail.com>
2024-03-24 00:43:42 +0900

    Update quiz2.c

--- quiz2.c
+++ quiz2.c
@@ -7,95 +7,99 @@
 #include <linux/hashtable.h>
 
 static int movies[][2] = {
-        {1997, 4},
-        {1980, 5},
-        {1983, 6},
-        {1999, 1},
-        {2002, 2},
-        {2005, 3},
-        {2015, 7},
-        {2017, 8}
+	{1997, 4},
+	{1980, 5},
+	{1983, 6},
+	{1999, 1},
+	{2002, 2},
+	{2005, 3},
+	{2015, 7},
+	{2017, 8}
 };
 
 static char title_temp[][100] = {
-        "Star Wars",
-        "The Empire Strikes Back",
-        "Return of the Jedi",
-        "Star Wars: Episode I - The Phantom Menace",
-        "Star Wars: Episode II - Attach of the Clones",
-        "Star Wars: Episode III - Revenge of the Sith",
-        "Star Wars: The Force Awakens",
-        "Star Wars: The Last Jedi"
+	"Star Wars",
+	"The Empire Strikes Back",
+	"Return of the Jedi",
+	"Star Wars: Episode I - The Phantom Menace",
+	"Star Wars: Episode II - Attach of the Clones",
+	"Star Wars: Episode III - Revenge of the Sith",
+	"Star Wars: The Force Awakens",
+	"Star Wars: The Last Jedi"
 };
 
 
 struct starwars_episode {
-    int year;    
-    int episode_number;
-    char title[100];    
-    struct hlist_node hash_node;
+	int year;    
+	int episode_number;
+	char title[100];    
+	struct hlist_node hash_node;
 };
 
 #define MAX_EPISODES 8
-DEFINE_HASHTABLE(episodes_table, 2); // 버킷 크기를 4로 설정
+
+DEFINE_HASHTABLE(episodes_table, 2); //bucket size set to 4
 
 int kernel_init(void)
 {
-    int i;
-    struct starwars_episode *episode;
+	int i;
+	struct starwars_episode *episode;
 
-    // 해시 테이블 초기화
-    hash_init(episodes_table);
+	hash_init(episodes_table);
 
-    // 스타워즈 에피소드 정보를 해시 테이블에 추가
-    for (i = 0; i < MAX_EPISODES; i++) {
-        episode = kmalloc(sizeof(*episode), GFP_KERNEL);
-        if (!episode)
-            return -ENOMEM;
- 
-        episode->year = movies[i][0];
-        episode->episode_number = movies[i][1];       
-        strcpy(episode->title,title_temp[i]);
-        
-        // 에피소드 숫자를 해시 값으로 사용하여 해시 테이블에 추가
-        hash_add(episodes_table, &episode->hash_node, episode->episode_number);
-    }
+	for (i = 0; i < MAX_EPISODES; i++) {
+		
+		episode = kmalloc(sizeof(*episode), GFP_KERNEL);
 
-    // 해시 테이블을 순회하며 버킷 별로 에피소드 정보 출력
-    struct hlist_node *tmp;
-    struct starwars_episode *e;
-    int bkt = 0;
-/*  for (bkt = 0; bkt < 4; bkt++) { 
-        hash_for_each_possible(episodes_table, e, hash_node, bkt) {
-            printk(KERN_INFO "Opening year : %d, Title : %s, Episode number : %d, bkt : %d\n"\
-                ,e->year, e->title, e->episode_number,bkt);
-        }
-    }
-*/         
-     hash_for_each(episodes_table, bkt, e, hash_node) {
-        printk(KERN_INFO "Opening year : %d, Title : %s, Episode number : %d, bkt : %d\n"\
-                ,e->year, e->title, e->episode_number,bkt);
-        }
-    return 0;
+		if (!episode)
+			return -ENOMEM;
+
+		episode->year = movies[i][0];
+		episode->episode_number = movies[i][1];       
+		strcpy(episode->title,title_temp[i]);
+	
+		hash_add(episodes_table, &episode->hash_node, episode->episode_number);
+	}
+
+	struct hlist_node *tmp;
+	struct starwars_episode *e;
+
+	int bkt = 0;
+
+	hash_for_each(episodes_table, bkt, e, hash_node) {
+		pr_info("Opening year : %d, Title : %s, Episode number : %d, bkt : %d\n"\
+		,e->year, e->title, e->episode_number,bkt);
+	} 
+
+	hash_for_each(episodes_table, bkt, e, hash_node) {
+
+		if (e->episode_number == 1 || e->episode_number == 8) {
+			pr_info("Opening year : %d, Title : %s, Episode number : %d, bkt : %d\n"\
+			,e->year, e->title, e->episode_number,bkt);
+		}	
+	}
+
+	return 0;
 }
 
 void kernel_exit(void)
 {
-   struct starwars_episode *e;
-    struct hlist_node *tmp;
-    int bkt;
+	struct starwars_episode *e;
+	struct hlist_node *tmp;
+	int bkt;
 
-    // 해시 테이블을 순회하며 각 버킷의 모든 요소를 제거하고 메모리 해제
-    hash_for_each_safe(episodes_table, bkt, tmp, e, hash_node) {
-        hash_del(&e->hash_node);
-        kfree(e);
-    }
+	hash_for_each_safe(episodes_table, bkt, tmp, e, hash_node) {
+		hash_del(&e->hash_node);
+		kfree(e);
+	}
 
-    printk(KERN_INFO "Removing the Starwars module\n");
-
+	pr_info("Removing the Starwars module\n");
 }
 
 module_init(kernel_init);
 module_exit(kernel_exit);
 
 MODULE_LICENSE("GPL");  
+
+
+
