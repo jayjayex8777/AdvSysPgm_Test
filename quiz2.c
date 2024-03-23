3bff80a50590c20bc3488a6e936dea05928afc7e
Intae Jun <jayjayex8777@gmail.com>
2024-03-24 00:28:59 +0900

    Update quiz2.c

--- quiz2.c
+++ quiz2.c
@@ -71,8 +71,8 @@ int kernel_init(void)
                 ,e->year, e->title, e->episode_number,bkt);
         }
     }
-*/
-        hash_for_each(episodes_table, bkt, tmp, e, hash_node) {
+*/        
+     hash_for_each(episodes_table, bkt, e, hash_node) {
         printk(KERN_INFO "Opening year : %d, Title : %s, Episode number : %d, bkt : %d\n"\
                 ,e->year, e->title, e->episode_number,bkt);
                 bkt++;
