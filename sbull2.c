a921f71772b57c3f3ac92b10104ad44f82e091da
Intae Jun <jayjayex8777@gmail.com>
2024-04-08 01:07:45 +0900

    Update sbull2.c

--- sbull2.c
+++ sbull2.c
@@ -1,176 +1,230 @@
+
+
+
+/*
+ * Sample disk driver, from the beginning.
+ */
+
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/init.h>
 
 #include <linux/sched.h>
-#include <linux/kernel.h>
-#include <linux/slab.h>
-#include <linux/fs.h>
-#include <linux/errno.h>
+#include <linux/kernel.h>       /* printk() */
+#include <linux/slab.h>         /* kmalloc() */
+#include <linux/fs.h>           /* everything... */
+#include <linux/errno.h>        /* error codes */
 #include <linux/timer.h>
-#include <linux/types.h>
-#include <linux/fcntl.h>
-#include <linux/hdreg.h>
+#include <linux/types.h>        /* size_t */
+#include <linux/fcntl.h>        /* O_ACCMODE */
+#include <linux/hdreg.h>        /* HDIO_GETGEO */
 #include <linux/kdev_t.h>
 #include <linux/vmalloc.h>
 #include <linux/blkdev.h>
-#include <linux/buffer_head.h>
+#include <linux/buffer_head.h>  /* invalidate_bdev */
 #include <linux/bio.h>
-#include <linux/list.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
 
 static int sbull_major = 0;
 static int hardsect_size = 512;
-static int nsectors = 1024 * 1024; // How big the drive is
+static int nsectors = 1024 * 1024;      /* How big the drive is */
 
-#define KERNEL_SECTOR_SIZE 512
+/*
+ * We can tweak our hardware sector size, but the kernel talks to us
+ * in terms of small sectors, always.
+ */
+#define KERNEL_SECTOR_SIZE      512
 
+/*
+ * The internal representation of our device.
+ */
 struct sbull_dev {
-    int size;
-    int users;
-    spinlock_t lock;
-    struct gendisk *gendisk;
-    struct list_head data_list; // Head of the linked list for data management
+        int size;
+        int users;
+        spinlock_t lock;
+
+        struct gendisk *gendisk;
 };
 
 static struct sbull_dev device;
 
-struct sbull_data {
-    struct list_head list;
-    unsigned long sector; // Sector number
-    void *data; // Data buffer
+struct sbull_list {
+        unsigned long idx;
+        char buf[4096];
+        // TODO: You can add data needed by the data structure of your choice
 };
+// TODO: You can declare global variables too
 
-static inline unsigned int bio_cur_bytes(struct bio *bio) {
-    if (bio_has_data(bio))
-        return bio_iovec(bio).bv_len;
-    else /* dataless requests such as discard */
-        return bio->bi_iter.bi_size;
+static inline unsigned int bio_cur_bytes(struct bio *bio)
+{
+        if (bio_has_data(bio))
+                return bio_iovec(bio).bv_len;
+        else /* dataless requests such as discard */
+                return bio->bi_iter.bi_size;
 }
 
 static void sbull_transfer(struct sbull_dev *dev, unsigned long sector,
-                           unsigned long nsect, char *buffer, int write) {
-    struct sbull_data *ptr, *next;
-    unsigned long offset = sector * KERNEL_SECTOR_SIZE;
-    unsigned long nbytes = nsect * KERNEL_SECTOR_SIZE;
-    bool found = false;
+                           unsigned long nsect, char *buffer, int write)
+{
+        unsigned long offset = sector * KERNEL_SECTOR_SIZE;
+        unsigned long nbytes = nsect * KERNEL_SECTOR_SIZE;
+        struct sbull_list *ptr;
 
-    list_for_each_entry_safe(ptr, next, &dev->data_list, list) {
-        if (ptr->sector == sector) {
-            if (write)
-                memcpy(ptr->data, buffer, nbytes);
-            else
-                memcpy(buffer, ptr->data, nbytes);
-            found = true;
-            break;
+        if ((offset + nbytes) > dev->size) {
+                pr_err("Beyond-end write (%ld %ld)\n", offset, nbytes);
+                return;
         }
-    }
-    
-    if (!found && write) {
-        ptr = kmalloc(sizeof(struct sbull_data), GFP_KERNEL);
-        if (!ptr) {
-            pr_err("sbull: Out of memory\n");
-            return;
+
+        // TODO: Memory allocation, data structure management, kmalloc, memcpy, etc...
+}
+
+/*
+ * Transfer a single BIO.
+ */
+static int sbull_xfer_bio(struct sbull_dev *dev, struct bio *bio)
+{
+        struct bvec_iter iter;
+        struct bio_vec bvec;
+        sector_t sector = bio->bi_iter.bi_sector;
+
+        // Process each and every segment
+        bio_for_each_segment(bvec, bio, iter) {
+                // Map a kernel page for I/O
+                char *buffer = kmap_atomic(bvec.bv_page) + bvec.bv_offset;
+                // Read from or write to the buffer
+                sbull_transfer(dev, sector, bio_cur_bytes(bio) >> 9, buffer, bio_data_dir(bio) == WRITE);
+                sector += bio_cur_bytes(bio) >> 9;
+                // Free the mapped kernel page
+                kunmap_atomic(buffer);
         }
-        ptr->data = kmalloc(nbytes, GFP_KERNEL);
-        if (!ptr->data) {
-            kfree(ptr);
-            pr_err("sbull: Out of memory for data\n");
-            return;
-        }
-        memcpy(ptr->data, buffer, nbytes);
-        ptr->sector = sector;
-        list_add_tail(&ptr->list, &dev->data_list);
-    }
+
+        return 0;               /* Always "succeed" */
 }
 
-static int sbull_xfer_bio(struct sbull_dev *dev, struct bio *bio) {
-    struct bio_vec bvec;
-    struct bvec_iter iter;
-    bio_for_each_segment(bvec, bio, iter) {
-        char *buffer = kmap_atomic(bvec.bv_page) + bvec.bv_offset;
-        sbull_transfer(dev, bio->bi_iter.bi_sector, bio_cur_bytes(bio) >> 9, buffer, bio_data_dir(bio) == WRITE);
-        kunmap_atomic(buffer);
-    }
-    return 0; // Always "succeed"
+/*
+ * The direct make request version.
+ */
+static void sbull_make_request(struct bio *bio)
+{
+        struct sbull_dev *dev = bio->bi_bdev->bd_disk->queue->queuedata;
+        int status;
+
+        status = sbull_xfer_bio(dev, bio);
+        bio_endio(bio);
 }
 
-static void sbull_make_request(struct bio *bio) {
-    struct sbull_dev *dev = bio->bi_bdev->bd_disk->queue->queuedata;
-    sbull_xfer_bio(dev, bio);
-    bio_endio(bio);
+/*
+ * Open and close.
+ */
+/* changed open and release function */
+
+static int sbull_open(struct gendisk *disk, fmode_t mode)
+{
+        struct sbull_dev *dev = disk->private_data;
+
+        spin_lock(&dev->lock);
+        dev->users++;
+        spin_unlock(&dev->lock);
+        
+        return 0;
 }
 
-static int sbull_open(struct gendisk *disk, fmode_t mode) {
-    struct sbull_dev *dev = disk->private_data;
-    spin_lock(&dev->lock);
-    dev->users++;
-    spin_unlock(&dev->lock);
-    return 0;
+static void sbull_release(struct gendisk *disk)
+{
+        struct sbull_dev *dev = disk->private_data;
+
+        spin_lock(&dev->lock);
+        dev->users--;
+        spin_unlock(&dev->lock);
 }
 
-static void sbull_release(struct gendisk *disk, fmode_t mode) {
-    struct sbull_dev *dev = disk->private_data;
-    spin_lock(&dev->lock);
-    dev->users--;
-    spin_unlock(&dev->lock);
-}
-
+/*
+ * The device operations structure.
+ */
 static struct block_device_operations sbull_ops = {
-    .owner = THIS_MODULE,
-    .open = sbull_open,
-    .release = sbull_release,
+        .open = sbull_open,
+        .release = sbull_release,
+        .submit_bio = sbull_make_request,
 };
 
-static void setup_device(struct sbull_dev *dev) {
-    memset(dev, 0, sizeof(*dev));
-    dev->size = nsectors * hardsect_size;
-    spin_lock_init(&dev->lock);
-    INIT_LIST_HEAD(&dev->data_list);
+/*
+ * Set up our internal device.
+ */
+static noinline void setup_device(struct sbull_dev *dev)
+{
+        int ret;
 
-    dev->gendisk = blk_alloc_disk(NUMA_NO_NODE);
-    if (!dev->gendisk) {
-        pr_info("alloc_disk failure\n");
+        memset(dev, 0, sizeof(struct sbull_dev));
+        dev->size = nsectors * hardsect_size;
+        spin_lock_init(&dev->lock);     /* Initialize spinlock */
+
+        /* gendisk structure */
+        dev->gendisk = blk_alloc_disk(NUMA_NO_NODE);
+        if (!dev->gendisk) {
+                pr_info("alloc_disk failure\n");
+                return;
+        }
+
+        dev->gendisk->queue->queuedata = dev;
+
+        /*
+         * And the gendisk structure.
+         */
+        dev->gendisk->major = sbull_major;
+        dev->gendisk->first_minor = 0;
+        dev->gendisk->minors = 1;
+        dev->gendisk->fops = &sbull_ops;
+        dev->gendisk->private_data = dev;       /* register the private data structure */
+
+        snprintf(dev->gendisk->disk_name, 32, "sbull0");
+
+        /*
+         * To ensure that we always get PAGE_SIZE aligned
+         * and n*PAGE_SIZED sized I/O requests.
+         */
+        blk_queue_physical_block_size(dev->gendisk->queue, PAGE_SIZE);
+        blk_queue_logical_block_size(dev->gendisk->queue, 1 << 12);
+        blk_queue_io_min(dev->gendisk->queue, PAGE_SIZE);
+
+        set_capacity(dev->gendisk, nsectors * (hardsect_size / KERNEL_SECTOR_SIZE));
+
+        ret = add_disk(dev->gendisk);
+        if (ret != 0) {
+                pr_err("Failed to add sbull device: %d\n", ret);
+        }
+
         return;
-    }
-
-    dev->gendisk->queue = blk_alloc_queue(GFP_KERNEL);
-    blk_queue_make_request(dev->gendisk->queue, sbull_make_request);
-    dev->gendisk->major = sbull_major;
-    dev->gendisk->first_minor = 0;
-    dev->gendisk->minors = 1;
-    dev->gendisk->fops = &sbull_ops;
-    dev->gendisk->private_data = dev;
-    snprintf(dev->gendisk->disk_name, 32, "sbull0");
-    set_capacity(dev->gendisk, nsectors);
-    add_disk(dev->gendisk);
 }
 
-static int __init sbull_init(void) {
-    sbull_major = register_blkdev(0, "sbull");
-    if (sbull_major <= 0) {
-        pr_err("sbull: unable to get major number\n");
-        return -EBUSY;
-    }
+static int __init sbull_init(void)
+{
+        /*
+         * Get registered.
+         */
+        sbull_major = register_blkdev(0, "sbull");
+        if (sbull_major <= 0) {
+                pr_err("sbull: unable to get major number\n");
+                return -EBUSY;
+        }
 
-    setup_device(&device);
-    return 0;
+        setup_device(&device);
+
+        // TODO: You can add data structure initialization here if needed
+
+        return 0;
 }
 
-static void __exit sbull_exit(void) {
-    struct sbull_dev *dev = &device;
-    struct sbull_data *tmp, *next;
+static void sbull_exit(void)
+{
+        struct sbull_dev *dev = &device;
 
-    list_for_each_entry_safe(tmp, next, &dev->data_list, list) {
-        list_del(&tmp->list);
-        kfree(tmp->data);
-        kfree(tmp);
-    }
+        if (dev->gendisk) {
+                del_gendisk(dev->gendisk);
+                put_disk(dev->gendisk);
+        }
 
-    del_gendisk(dev->gendisk);
-    put_disk(dev->gendisk);
-    unregister_blkdev(sbull_major, "sbull");
+        unregister_blkdev(sbull_major, "sbull");
 }
 
 module_init(sbull_init);
