5ec422c16e52b1aa7c9470cb4b7abb8a2a58c67b
Intae Jun <jayjayex8777@gmail.com>
2024-04-17 22:17:20 +0900

    Update lab7_quiz2.c

--- lab7_quiz2.c
+++ lab7_quiz2.c
@@ -1,3 +1,5 @@
+
+
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
@@ -30,22 +32,53 @@ sbuf_t *sbufs = NULL;
 static int producer(void *arg)
 {
         // insert 0-29 integers
+          int i;
+    for (i = 0; i < ITEMS; i++) {
+        sbuf_insert(sbufs, i);
+        printk(KERN_INFO "Inserted %d\n", i);
+    }
+    return 0;
 }
 
 static int consumer(void *arg)
 {
         // remove 30 items and print each item
+           int i, item;
+    for (i = 0; i < ITEMS; i++) {
+        item = sbuf_remove(sbufs);
+        printk(KERN_INFO "Removed %d\n", item);
+    }
+    return 0;
 }
 
 static int simple_init(void)
 {
         // init 1 sbuf
         // create 1 producer, 1 consumer
+        printk(KERN_INFO "Initializing sbuf example module\n");
+    sbufs = kmalloc(sizeof(sbuf_t), GFP_KERNEL);
+    if (!sbufs) {
+        printk(KERN_ERR "Failed to allocate memory for sbufs\n");
+        return -ENOMEM;
+    }
+    sbuf_init(sbufs, SBUFSIZE);
+
+    pthreads = kthread_run(producer, NULL, "producer_thread");
+    cthreads = kthread_run(consumer, NULL, "consumer_thread");
+
+    return 0;
 }
 
 static void simple_exit(void)
 {
         // deinit sbuf
+        if (pthreads) kthread_stop(pthreads);
+    if (cthreads) kthread_stop(cthreads);
+    if (sbufs) {
+        sbuf_deinit(sbufs);
+        kfree(sbufs);
+    }
+    printk(KERN_INFO "Exiting sbuf example module\n");
 }
 
 module_init(simple_init);
@@ -54,4 +87,6 @@ module_exit(simple_exit);
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Simple Module");
 MODULE_AUTHOR("KOO");
+
+
 
