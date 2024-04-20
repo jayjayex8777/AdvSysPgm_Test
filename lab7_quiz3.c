de33b36e2e63717e6618e2765cc3ad9e7b8f177f
Intae Jun <jayjayex8777@gmail.com>
2024-04-20 14:43:19 +0900

    Update lab7_quiz3.c

--- lab7_quiz3.c
+++ lab7_quiz3.c
@@ -34,60 +34,67 @@ sbuf_t *sbufs = NULL;
 
 static int producer(void *arg)
 {
-    int i, buf_index;
-    for (i = 0; i < 15; i++) {
-       get_random_bytes(&buf_index, sizeof(buf_index));
-       
-       if(buf_index < 0){
-          buf_index = - buf_index;
-       }
-       
-       buf_index = buf_index % NUM_SBUF;
-       
-       sbuf_insert(&sbufs[buf_index], i);
-       for(int j;j<SBUFSIZE;j++){
-          pr_info("Cnt %d : Inserted %d \n",i,*(sbufs[buf_index]->buf[j]));
-       }
-    }
+	int i, buf_index;
 
-    pr_info("Producer Done\n");
-    
-    return 0;
+	for (i = 0; i < 15; i++) {
+		get_random_bytes(&buf_index, sizeof(buf_index));
+
+		if(buf_index < 0){
+			buf_index = - buf_index;
+		}
+
+		buf_index = buf_index % NUM_SBUF; 
+
+		sbuf_insert(&sbufs[buf_index], i);
+	}
+
+	pr_info("Producer Done\n");
+
+	return 0;
 }
 
 static int consumer(void *arg)
 {
     int item, count = 0;
-    while (count < ITEMS) {
-        int buf_index;
-        for (buf_index = 0; buf_index < NUM_SBUF; buf_index++) {
-            item = sbuf_tryremove(&sbufs[buf_index]);
-            if (item != -1) {
-                pr_info("Consumed: %d from buffer %d\n", item, buf_index);
-                count++;
-                if (count >= ITEMS)
-                    break;
-            }
-        }
-    }
+	
+	while (count < ITEMS) {
+		int buf_index;
+
+		for (buf_index = 0; buf_index < NUM_SBUF; buf_index++) {
+
+			item = sbuf_tryremove(&sbufs[buf_index]);
+
+			if (item != -1) {
+				pr_info("Consumed: %d from buffer %d\n", item, buf_index);
+
+				count++;
+
+				if (count >= ITEMS)
+					break;
+			}
+		}	
+	}
+	
     return 0;
 }
 
 static int simple_init(void)
 {
     int i;
-    sbufs = kmalloc(NUM_SBUF * sizeof(sbuf_t), GFP_KERNEL);
-    for (i = 0; i < NUM_SBUF; i++) {
-        sbuf_init(&sbufs[i], SBUFSIZE);
-    }
 
-    for (i = 0; i < NUM_THREADS; i++) {
-        pthreads[i] = kthread_run(producer, NULL, "producer_thread_%d", i);
-    }
-   if(0)
-    cthread = kthread_run(consumer, NULL, "consumer_thread");
+	sbufs = kmalloc(NUM_SBUF * sizeof(sbuf_t), GFP_KERNEL);
 
-    return 0;
+	for (i = 0; i < NUM_SBUF; i++) {
+		sbuf_init(&sbufs[i], SBUFSIZE);
+	}
+
+	for (i = 0; i < NUM_THREADS; i++) {
+		pthreads[i] = kthread_run(producer, NULL, "producer_thread_%d", i);
+	}
+
+	cthread = kthread_run(consumer, NULL, "consumer_thread");
+
+	return 0;
 }
 
 static void simple_exit(void)
@@ -96,15 +103,19 @@ static void simple_exit(void)
     for (i = 0; i < NUM_SBUF; i++) {
         sbuf_deinit(&sbufs[i]);
     }
+	
     kfree(sbufs);
-   #if 0
+	pr_info("sbufs freed all\n");
+	
+#if 0
     for (i = 0; i < NUM_THREADS; i++) {
         if (pthreads[i])
             kthread_stop(pthreads[i]);
     }
     if (cthread)
         kthread_stop(cthread);
-   #endif
+#endif
+
 }
 
 module_init(simple_init);
