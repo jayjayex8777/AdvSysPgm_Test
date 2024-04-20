387817d23f525d26e983f021c1c03de9549bed9f
Intae Jun <jayjayex8777@gmail.com>
2024-04-20 14:10:12 +0900

    Update lab7_quiz3.c

--- lab7_quiz3.c
+++ lab7_quiz3.c
@@ -7,54 +7,79 @@
 
 #include "sbuf.h"
 
-/* create an empty, bounded, shared FIFO buffer with n slots */
-extern void sbuf_init(sbuf_t * sp, int n);
-
-/* clean up buffer sp */
-extern void sbuf_deinit(sbuf_t * sp);
-
-/* insert item onto the rear of shared buffer sp */
-extern void sbuf_insert(sbuf_t * sp, int item);
-
-/* remove and return the first item from buffer sp */
-extern int sbuf_remove(sbuf_t * sp);
-
-/* if there is items in buffer, remove and return the first item from buffer sp.
-   if not, return -1, immediately */
-extern int sbuf_tryremove(sbuf_t * sp);
-
 #define ITEMS 30
 #define SBUFSIZE 3
 #define NUM_SBUF 4
 #define NUM_THREADS 2
 
-static struct task_struct **pthreads;
-static struct task_struct **cthreads;
-sbuf_t *sbufs = NULL;
+static struct task_struct *pthreads[NUM_THREADS];
+static struct task_struct *cthread;
+static sbuf_t *sbufs;
 
 static int producer(void *arg)
 {
-        // insert 0-14 integers to a queue
-        // the queue is randomly seledted
-   return 0;
+    int i, buf_index;
+    for (i = 0; i < 15; i++) {
+       get_random_bytes(&buf_index, sizeof(buf_index));
+       
+       if(buf_index < 0)
+          buf_index = buf_index % NUM_SBUF;
+       
+       sbuf_insert(&sbufs[buf_index], i);
+    }
+    return 0;
 }
 
 static int consumer(void *arg)
 {
-        // remove 30 items and print each item
-   return 0;
+    int item, count = 0;
+    while (count < ITEMS) {
+        int buf_index;
+        for (buf_index = 0; buf_index < NUM_SBUF; buf_index++) {
+            item = sbuf_tryremove(&sbufs[buf_index]);
+            if (item != -1) {
+                pr_info("Consumed: %d from buffer %d\n", item, buf_index);
+                count++;
+                if (count >= ITEMS)
+                    break;
+            }
+        }
+    }
+    return 0;
 }
 
 static int simple_init(void)
 {
-        // init 4 sbuf
-        // create 2 producer, 1 consumer
-   return 0;
+    int i;
+    sbufs = kmalloc(NUM_SBUF * sizeof(sbuf_t), GFP_KERNEL);
+    for (i = 0; i < NUM_SBUF; i++) {
+        sbuf_init(&sbufs[i], SBUFSIZE);
+    }
+
+    for (i = 0; i < NUM_THREADS; i++) {
+        pthreads[i] = kthread_run(producer, NULL, "producer_thread_%d", i);
+    }
+   if(0)
+    cthread = kthread_run(consumer, NULL, "consumer_thread");
+
+    return 0;
 }
 
 static void simple_exit(void)
 {
-        // deinit sbuf
+    int i;
+    for (i = 0; i < NUM_SBUF; i++) {
+        sbuf_deinit(&sbufs[i]);
+    }
+    kfree(sbufs);
+   #if 0
+    for (i = 0; i < NUM_THREADS; i++) {
+        if (pthreads[i])
+            kthread_stop(pthreads[i]);
+    }
+    if (cthread)
+        kthread_stop(cthread);
+   #endif
 }
 
 module_init(simple_init);
