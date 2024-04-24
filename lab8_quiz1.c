52a4164453c7ede17d95a0189a384f55c168cc48
Intae Jun <jayjayex8777@gmail.com>
2024-04-24 13:42:46 +0900

    Update lab8_quiz1.c

--- lab8_quiz1.c
+++ lab8_quiz1.c
@@ -1,58 +1,60 @@
-/*
- *   irq_ex1.c: An interrupt handler example. This code binds itself to `IRQ` 1, which
- *   is the IRQ of the keyboard controlled under Intel architectures. Then, when it
- *   receives a keyboard interrupt, it reads the keyboard's status and the scan code,
- *   which is the value return by the keyboard. And then, it puts information about
- *   Key that pressed. This example only has 3 key: ESC, F1 and F2
- */
-
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/sched.h>
-#include <linux/workqueue.h>
+#include <linux/init.h>
 #include <linux/interrupt.h>
 #include <asm/io.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("22SYS");
+MODULE_DESCRIPTION("Keyboard interrupt handler module");
 
+#define KEYBOARD_IRQ 1
+
+/*
+ * IRQ handler - This function will be called on keyboard interrupts
+ */
 irqreturn_t irq_handler(int irq, void *dev_id)
 {
-        /*
-         * This variables are static because they need to be
-         * accessible (through pointers) to the bottom half routine.
-         * ESC: 0x01
-         * F2: 0x3C
-         * F3: 0x3D
-         */
+    static unsigned char scancode;
+    unsigned char status;
 
-        static unsigned char scancode;
-        unsigned char status;
+    // Read keyboard status and scancode from the keyboard controller
+    status = inb(0x64);
+    scancode = inb(0x60);
 
-        /*
-         * Read keyboard status
-         */
+    switch (scancode)
+    {
+        case 0x01:
+            printk(KERN_INFO "ESC key pressed\n");
+            break;
+        case 0x3B:
+            printk(KERN_INFO "F1 key pressed\n");
+            break;
+        case 0x3C:
+            printk(KERN_INFO "F2 key pressed\n");
+            break;
+    }
 
-        return IRQ_HANDLED;
+    return IRQ_HANDLED;
 }
 
 /*
- * Initialize the module - register the IRQ handler
+ * Module initialization
  */
 static int __init irq_ex_init(void)
 {
-        /*
-         * Request IRQ 1, the keyboard IRQ, to go to our irq_handler.
-         */
+    // Request shared IRQ 1 for the keyboard
+    return request_irq(KEYBOARD_IRQ, irq_handler, IRQF_SHARED, "keyboard_irq_handler", (void *)(irq_handler));
 }
+
+/*
+ * Module cleanup
+ */
 static void __exit irq_ex_exit(void)
 {
-        /*
-         * Free IRQ 1
-         */
+    // Free the IRQ and pass the same argument as in request_irq()
+    free_irq(KEYBOARD_IRQ, (void *)(irq_handler));
 }
 
 module_init(irq_ex_init);
 module_exit(irq_ex_exit);
-
-
