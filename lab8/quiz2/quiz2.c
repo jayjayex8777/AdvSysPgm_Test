9e92933943d38dfac75431d1c538fa5d6946a9a8
Intae Jun <jayjayex8777@gmail.com>
2024-04-28 21:41:18 +0900

    Update quiz2.c

--- lab8/quiz2/quiz2.c
+++ lab8/quiz2/quiz2.c
@@ -49,20 +49,20 @@ static DECLARE_TASKLET(my_exit_tasklet, do_exit_tasklet);
  */
 static void do_enqueue_tasklet(struct tasklet_struct *unused)
 {
-        pr_info("TASKLET You pressed F2\n");
-        enqueue_flag = true;
+    pr_info("TASKLET You pressed F2\n");
+    enqueue_flag = true;
 }
 
 static void do_dequeue_tasklet(struct tasklet_struct *unused)
 {
-        pr_info("TASKLET You pressed F3\n");
-        dequeue_flag = true;
+    pr_info("TASKLET You pressed F3\n");
+    dequeue_flag = true;
 }
 
 static void do_exit_tasklet(struct tasklet_struct *unused)
 {
-        pr_info("TASKLET You pressed ESC\n");
-        exit_flag = true;
+    pr_info("TASKLET You pressed ESC\n");
+    exit_flag = true;
 }
 
 
@@ -70,8 +70,7 @@ irqreturn_t irq_handler(int irq, void *dev_id)
 {
     static unsigned char scancode;
     unsigned char status;
-
-    // Read keyboard status and scancode from the keyboard controller
+    
     status = inb(0x64);
     scancode = inb(0x60);
 
@@ -79,17 +78,17 @@ irqreturn_t irq_handler(int irq, void *dev_id)
     {
         case 0x01:
             pr_info("! You pressed ESC ...\n");
-                tasklet_schedule(&my_exit_tasklet);
-                pr_info("! You pressed ESC ...\n");
-
+            tasklet_schedule(&my_exit_tasklet);
             break;
+
         case 0x3C:
             pr_info("! You pressed F2 ...\n");
-                tasklet_schedule(&my_enqueue_tasklet);
+            tasklet_schedule(&my_enqueue_tasklet);
             break;
+
         case 0x3D:
             pr_info("! You pressed F3 ...\n");
-                tasklet_schedule(&my_dequeue_tasklet);
+            tasklet_schedule(&my_dequeue_tasklet);
             break;
     }
 
@@ -98,121 +97,130 @@ irqreturn_t irq_handler(int irq, void *dev_id)
 
 static int producer(void *arg)
 {
-int val;
+    int val;
+    
     while (!kthread_should_stop()) {
-	    //while (!exit_flag) {
+        
         if (enqueue_flag) {
             sbuf_insert(sbufs, val);
-                pr_info("Producer enqueued item: %d\n",val);
-                val++;
+            
+            pr_info("Producer enqueued item: %d\n",val);
+            
+            val++;
+            
             enqueue_flag = 0;
         }
-        msleep(100); // Sleep to simulate work
+        
+        msleep(100);
 
-	if(exit_flag)
-	  break;
+        if(exit_flag)
+            break;
     }
-
-pr_info("Producer has terminated\n");
+    
+    pr_info("Producer has terminated\n");
+    
     return 0;
 }
 
 static int consumer(void *arg)
 {
-        int item;
-      while (!kthread_should_stop()) {
-  //while (!exit_flag) {
+    int item;
+    
+    while (!kthread_should_stop()) {
+    
         if (dequeue_flag) {
             item = sbuf_remove(sbufs);
-                pr_info("Consumer dequeued item: %d\n",item);
+            pr_info("Consumer dequeued item: %d\n",item);
             dequeue_flag = 0;
         }
+
         msleep(100);
 
         if(exit_flag)
-          break;
+            break;
     }
         
-        pr_info("Consumer has terminated\n");
+    pr_info("Consumer has terminated\n");
+    
     return 0;
 }
 
 static int simple_init(void)
 {
-        int ret;
-        
-        //pthreads = kmalloc(sizeof(struct task_struct *), GFP_KERNEL);
-        //cthreads = kmalloc(sizeof(struct tast_struct *), GFP_KERNEL);
-        sbufs = (sbuf_t *) kmalloc(sizeof(sbuf_t) * NUM_SBUF, GFP_KERNEL);
+    int ret;
 
-        sbuf_init(&sbufs[0], SBUFSIZE);
+    sbufs = (sbuf_t *) kmalloc(sizeof(sbuf_t) * NUM_SBUF, GFP_KERNEL);
+    sbuf_init(&sbufs[0], SBUFSIZE);
         
-        ret = request_irq(KEYBOARD_IRQ, irq_handler, IRQF_SHARED, "keyboard_irq_handler", (void *)(irq_handler));
+    ret = request_irq(KEYBOARD_IRQ, irq_handler, IRQF_SHARED, "keyboard_irq_handler", (void *)(irq_handler));
         
-        /* create a producer */
-        /* create a consumer */
-        pthreads = kthread_run(producer, NULL, "producer_thread");
-        if (IS_ERR(pthreads)) {
-                pr_err("Failed to create pthread\n");
-        } 
-        else {
-            pr_info("pthread created successfully\n");
-        }
+    /* create a producer */
+    /* create a consumer */
+    pthreads = kthread_run(producer, NULL, "producer_thread");
+    if (IS_ERR(pthreads)) {
+        pr_err("Failed to create pthread\n");
+    } 
+    else {
+        pr_info("pthread created successfully\n");
+    }
         
-        cthreads = kthread_run(consumer, NULL, "consumer_thread");
-        if (IS_ERR(cthreads)) {
-            pr_err("Failed to create cthread\n");
-        } 
-        else {
-            pr_info("cthread created successfully\n");
-        }
+    cthreads = kthread_run(consumer, NULL, "consumer_thread");
+    if (IS_ERR(cthreads)) {
+        pr_err("Failed to create cthread\n");
+    } 
+    else {
+        pr_info("cthread created successfully\n");
+    }
 
-        return ret;
+    return ret;
 }
 
 static void simple_exit(void)
 {
-	free_irq(KEYBOARD_IRQ, (void *)(irq_handler));
-	
-	pr_info("pthread %p , cthread %p\n",pthreads,cthreads);
+    pr_info("pthread %p , cthread %p\n",pthreads,cthreads);
 
-        if (pthreads){
-          if(!exit_flag){
-                kthread_stop(pthreads);
-                pr_info("pthread stopped successfully\n");
-                }
-          else 
-              pr_info("pthread already stopped\n");
+    if (pthreads){
+        if(!exit_flag){
+            kthread_stop(pthreads);
+            pr_info("pthread stopped successfully\n");
         }
-        if (cthreads){ 
-            if(!exit_flag){
-                kthread_stop(cthreads);  
-                pr_info("cthread stopped successfully\n");
-            }
-            else 
-              pr_info("cthread already stopped\n");
+        else 
+            pr_info("pthread already stopped\n");
         }
+    
+    if (cthreads){ 
+        if(!exit_flag){
+            kthread_stop(cthreads);  
+            pr_info("cthread stopped successfully\n");
+        }
+        else 
+            pr_info("cthread already stopped\n");
+    }
 
-        /*
-         * free irq, free tasklet
-         */        
-        tasklet_kill(&my_enqueue_tasklet);
+    /*
+    * free irq, free tasklet
+    */ 
+    free_irq(KEYBOARD_IRQ, (void *)(irq_handler));
+    pr_info("KBD IRQ Freed\n");
+
+    tasklet_kill(&my_enqueue_tasklet);
 	pr_info("my_enqueue_tasklet killed\n");
-        tasklet_kill(&my_dequeue_tasklet);
+    
+    tasklet_kill(&my_dequeue_tasklet);
 	pr_info("my_dequeue_tasklet killed\n");
-        tasklet_kill(&my_exit_tasklet);
+    
+    tasklet_kill(&my_exit_tasklet);
 	pr_info("my_exit_tasklet killed\n");
         
-        if(sbufs){
-	  pr_info("trying to free sbuf...\n");
-          kfree(sbufs);
-	  pr_info("sbuf freed\n");
+    if(sbufs){
+        kfree(sbufs);
+        pr_info("sbuf freed\n");
           sbufs = NULL;
-        }
+    }
 }
 
-module_init(simple_init);       // 모듈 생성될 때 simpel_init 함수 호출
-module_exit(simple_exit);       // 모듈 생성될 때 simpel_exit 함수 호출
+module_init(simple_init);
+module_exit(simple_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Simple Module");
