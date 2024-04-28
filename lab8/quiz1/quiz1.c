31c45ecb639a9c3c69f58881a06ad2710edf7a77
Intae Jun <jayjayex8777@gmail.com>
2024-04-28 23:18:38 +0900

    Update quiz1.c

--- lab8/quiz1/quiz1.c
+++ lab8/quiz1/quiz1.c
@@ -6,15 +6,11 @@
 
 #define KEYBOARD_IRQ 1
 
-/*
- * IRQ handler - This function will be called on keyboard interrupts
- */
 irqreturn_t irq_handler(int irq, void *dev_id)
 {
     static unsigned char scancode;
     unsigned char status;
 
-    // Read keyboard status and scancode from the keyboard controller
     status = inb(0x64);
     scancode = inb(0x60);
 
@@ -34,26 +30,19 @@ irqreturn_t irq_handler(int irq, void *dev_id)
     return IRQ_HANDLED;
 }
 
-/*
- * Module initialization
- */
 static int __init irq_ex_init(void)
 {
     int ret;
 
     pr_info("irq_ex_init done\n");
-    // Request shared IRQ 1 for the keyboard
+
     ret = request_irq(KEYBOARD_IRQ, irq_handler, IRQF_SHARED, "keyboard_irq_handler", (void *)(irq_handler));
 
     return ret;
 }
 
-/*
- * Module cleanup
- */
 static void __exit irq_ex_exit(void)
-{
-    // Free the IRQ and pass the same argument as in request_irq()
+{    
     free_irq(KEYBOARD_IRQ, (void *)(irq_handler));
     pr_info("irq_ex_exit done\n");
 }
@@ -62,5 +51,5 @@ module_init(irq_ex_init);
 module_exit(irq_ex_exit);
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("22SYS");
+MODULE_AUTHOR("Intae Jun");
 MODULE_DESCRIPTION("Keyboard interrupt handler module");
